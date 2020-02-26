8c1d5d7ef843e3190a3d5859d247ac3eb26788e5
Nicolai Andre Dalaaker <nicolaad@stud.ntnu.no>
2020-02-26 13:26:15 +0100

    functional authentication

--- backend/afk_backend/settings.py
+++ backend/afk_backend/settings.py
@@ -132,11 +132,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
+}
+
+JWT_AUTH = {
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'afk_backend.utils.my_jwt_response_handler'
 }
