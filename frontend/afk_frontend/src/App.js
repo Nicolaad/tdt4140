767c5aa3798784c369cbffde436178bbe60c149e
Vilde Taklo Kenworthy <vildetk@stud.ntnu.no>
2020-02-27 14:02:31 +0100

    added frontend for login

--- frontend/afk_frontend/src/App.js
+++ frontend/afk_frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import PostForm from "./PostForm";
+import Login from './Login';
 
-function App() {
-  return (
-    <div className="App">
-      <PostForm />
-    </div>
-  );
+class App extends Component{  
+
+  render(){
+    return (
+      <>
+      <div id = "header">
+        <div>
+        <button className = "login" onClick = {this.togglePop}> Logg inn</button>
+        <Login />
+        </div>
+        <button className = "register"> Registrer deg</button>
+      </div>
+      </>
+    );
+  }
 }
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
+)
 
 export default App;
