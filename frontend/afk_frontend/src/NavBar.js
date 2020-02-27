ebf6d50dba927057c84e53e7046cabcc72330656
Nicolai Andre Dalaaker <nicolaad@stud.ntnu.no>
2020-02-27 14:44:11 +0100

    added login into navbar (but needs some styling)

--- frontend/afk_frontend/src/NavBar.js
+++ frontend/afk_frontend/src/NavBar.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import Modal from "./Modal/Modal";
 import PostForm from "./RegisterUser/PostForm";
 import "./NavBar.css";
+import Login from "./Login"
  
 class NavBar extends Component {
   render() {
     return (
       <div className = "navBar">
         <div className = "login">
-            <Modal modalProps={{triggerText: "Logg inn"}} modalContent={<PostForm />} />
+            <Modal modalProps={{triggerText: "Logg inn"}} modalContent={<Login />} />
             <Modal modalProps={{triggerText: "Register bruker"}} modalContent={<PostForm />} />
         </div>
       </div>
