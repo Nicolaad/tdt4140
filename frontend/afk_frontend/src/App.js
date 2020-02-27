4493f45d45ad6b7a1255393a9194f4b76a55e07b
andrenge <andrenge@stud.ntnu.no>
2020-02-27 13:48:46 +0100

    Added Modal component andNavbar

--- frontend/afk_frontend/src/App.js
+++ frontend/afk_frontend/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
-import PostForm from "./RegisterUser/PostForm";
 import "./App.css";
-import Modal from "./Modal/Modal";
+import NavBar from "./NavBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Modal modalProps={{triggerText: "Logg inn"}} modalContent={<PostForm />} />
-        <Modal modalProps={{triggerText: "Register bruker"}} modalContent={<PostForm />} />
+        <NavBar />
       </div>
     );
   }
--- frontend/afk_frontend/src/Modal/ModalContent.js
+++ frontend/afk_frontend/src/Modal/ModalContent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import FocusTrap from 'focus-trap-react';
-// import focusTrap from 'focus-trap';
+import "./Modal.css";
 export class ModalContent extends Component {
   render() {
     return ReactDOM.createPortal(
