2e97aa6c129ad2ed0e54e58fdba2d567f41059f4
Nicolai Andre Dalaaker <nicolaad@stud.ntnu.no>
2020-02-29 18:07:47 +0100

    temp disabled ccs for login, for usabillity

--- frontend/afk_frontend/src/Login.js
+++ frontend/afk_frontend/src/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import axios from 'axios';
-import './Login.css';
+//import './Login.css'; temp removed, as it need fixing
 
 export default class Login extends Component{
     constructor(props) {
