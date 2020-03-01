e58892b1d3b89a1fcdc637a2a225c7ca5d16f330
andrenge <andrenge@stud.ntnu.no>
2020-03-01 14:40:57 +0100

    Added thread creation frontend

---
+++ frontend/afk_frontend/src/Thread/ThreadPost.js
@@ -0,0 +1,69 @@
+import React from "react";
+import axios from "axios";
+
+class ThreadPost extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: ''
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios
+            .post('http://127.0.0.1:8000/threads/', this.state)
+            .then(response => {
+                console.log(response)
+                if (response.status == 201) {
+                    alert("Thread posted");
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        const { title, text } = this.state;
+
+        return (
+            <div className="field">
+                <form onSubmit = {this.submitHandler} id="threadPost">
+                    <div>
+                        <h2>Tittel</h2>
+                        <input 
+                        type="text" 
+                        name="title" 
+                        value = {title}
+                        onChange = {this.changeHandler}/>
+                    </div>
+                </form>
+                <textarea
+                    rows="12"
+                    cols="70"
+                    name="threadContent"
+                    form="threadPost"
+                    placeholder="Skriv her..."
+                    id = "threadContent"
+                    onChange= {this.changeHandler}>{text}
+                </textarea>
+                <button 
+                    className = "register-button" 
+                    type="submit" 
+                    form="threadPost">
+                    Publiser
+                </button>
+            </div>
+        )
+    }
+}
+
+export default ThreadPost
