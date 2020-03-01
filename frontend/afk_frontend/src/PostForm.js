2a53c5d26c1638efeb8e0f8292be342c831c9ec1
Kenny Nguyen <Kenny.van96@gmail.com>
2020-03-01 14:11:18 +0100

    Modified PostForm.js and Postform.css

--- frontend/afk_frontend/src/PostForm.js
+++ frontend/afk_frontend/src/PostForm.js
@@ -25,7 +25,7 @@ class PostForm extends React.Component {
             .post('http://127.0.0.1:8000/users/', this.state)
             .then(response => {
                 console.log(response)
-                if (response.status == 201) {
+                if (response.status === 201) {
                     alert("User Created");
                 }
             })
@@ -38,34 +38,43 @@ class PostForm extends React.Component {
         const { username, email, password } = this.state;
         return (
             <div className="field">
-                <h1>Registrer bruker</h1>
-                <form onSubmit = {this.submitHandler}>
-                    <div>
-                        <h2>Brukernavn</h2>
-                        <input 
-                        type="text" 
-                        name="username" 
-                        value = {username}
-                        onChange = {this.changeHandler}/>
-                    </div>
-                    <div>
-                        <h2>Email</h2>
-                        <input 
-                        type="text" 
-                        name="email" 
-                        value = {email}
-                        onChange = {this.changeHandler}/>
-                    </div>
-                    <div>
-                        <h2>Passord</h2>
-                        <input 
-                        type="password" 
-                        name="password" 
-                        value = {password}
-                        onChange = {this.changeHandler}/>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
+                <div className="field-wrapper">
+                    <h1>Registrer bruker</h1>
+                    <form onSubmit = {this.submitHandler}>
+                        <div className="username">
+                            <label htmlFor="username">Brukernavn</label>
+                            <input 
+                                type="text"
+                                placeholder="Brukernavn"
+                                name="username" 
+                                value = {username}
+                                onChange={this.changeHandler}
+                            />
+                        </div>
+                        <div className="email">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="text"
+                                placeholder="Email"
+                                name="email" 
+                                value = {email}
+                                onChange={this.changeHandler}
+                            />
+                        </div>
+                        <div className="password">
+                            <label htmlFor="password">Passord</label>
+                            <input 
+                                type="password" 
+                                placeholder="Passord"
+                                name="password" 
+                                value = {password}
+                                onChange={this.changeHandler}
+                            />
+                        </div>
+                        <button className="buttonfx" type="submit">Opprett bruker</button>
+                        <button className="alreadyUser" type="submit">Allerede opprettet en bruker?</button>
+                    </form>
+                </div>
             </div>
         )
     }
