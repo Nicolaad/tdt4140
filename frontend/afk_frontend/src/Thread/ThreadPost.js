c648c50fcd8533ce180b0d9a1f5d36bb14ee1e71
Nicolai Andre Dalaaker <nicolaad@stud.ntnu.no>
2020-03-22 13:10:04 +0100

    removed un-needed comment

--- frontend/afk_frontend/src/Thread/ThreadPost.js
+++ frontend/afk_frontend/src/Thread/ThreadPost.js
@@ -24,13 +24,12 @@ class ThreadPost extends React.Component {
 
             Authorization: 'JWT '+ localStorage.getItem('token')
          }}
-         //swapp out this.state below with testComment to make a test comment
         axios
             .post('http://127.0.0.1:8000/threads/', this.state, token)
             .then(response => {
                 console.log(response)
                 if (response.status == 201) {
-                    //close popupp by calling the the close modal button
+                    //close pop-up by calling the the close modal button
                     document.getElementsByClassName("_modal-close")[0].click()
                     this.props.updateThreads()
                     
