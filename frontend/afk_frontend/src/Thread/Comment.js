2d71ebcb68e37fabb497f9ad57f2469a24310afe
Nicolai Andre Dalaaker <nicolaad@stud.ntnu.no>
2020-03-22 11:58:42 +0100

    changed date format to display a more readable time

--- frontend/afk_frontend/src/Thread/Comment.js
+++ frontend/afk_frontend/src/Thread/Comment.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./Comment.css";
 
 function Comment(props){
+    const date = new Date(props.date)
     return(
         <div className="comment">
             <div className="commentMetaData">
             <p>{props.username}</p>
-            <p>{props.date}</p>
+            <p>{date.getDate() + "/" + date.getMonth() + "/"+ date.getFullYear()+ ", " + date.getHours() +":"+ date.getMinutes()}</p>
             </div>
             <p>{props.postContent}</p>
         </div>
--- frontend/afk_frontend/src/Thread/Thread.js
+++ frontend/afk_frontend/src/Thread/Thread.js
@@ -4,12 +4,13 @@ import "./Thread.css";
 
 
 function Thread(props) {
+    const date = new Date(props.dateCreated)
         return (
             <div className="threadBody">
 
                 <h2>{props.ownername}</h2>
                 <h2>{props.title}</h2>
-            <p>dato: {props.dateCreated}</p>
+            <p>{date.getDate() + "/" + date.getMonth() + "/"+ date.getFullYear()+ ", " + date.getHours() +":"+ date.getMinutes()}</p>
                 <p>
                     {props.postContent}
                 </p>
